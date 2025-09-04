20b947fb5cdfad550bbca0ff09d26ad03be990b2
Votre Nom <cedric.franchini@gmail.com>
2025-09-04 20:23:29 +0200

    .

---
+++ .sassrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  quietDeps: true,
+  future: {
+    enableDeprecationWarnings: false,
+  },
+  loadPaths: ["node_modules", "_sass"],
+  sourceMap: false,
+  style: "compressed"
+};
