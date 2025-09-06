f60ad42217aaf8696bf1104c908280a40c1b7546
Votre Nom <cedric.franchini@gmail.com>
2025-09-06 06:56:40 +0200

    .

--- serveur/start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Ce script lance le panneau de contrôle du serveur Jekyll.
-
-REM Définit le chemin du script PowerShell
-SET SCRIPTPATH=%~dp0control-panel.ps1
-
-REM Lance le script PowerShell
-echo Lancement du panneau de controle...
-powershell -ExecutionPolicy Bypass -File "%SCRIPTPATH%"
